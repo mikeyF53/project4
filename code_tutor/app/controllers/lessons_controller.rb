3ba0fd4b5022d44c4cfdda646355a8e55652adc8
MikeyF53 <mikefang530@gmail.com>
2019-04-08 11:12:02 -0400

    added path for lesson controller

--- code_tutor/app/controllers/lessons_controller.rb
+++ code_tutor/app/controllers/lessons_controller.rb
@@ -4,17 +4,19 @@ class LessonsController < ApplicationController
   # GET /lessons
   def index
     @lessons = Lesson.all
-
     render json: @lessons
   end
 
-  # GET /lessons/1
+  # GET /user/user_id/lessons/lesson_id
   def show
+    @user = User.find(params[:user_id])
+    @lesson = Lesson.find(params[:lesson_id])
     render json: @lesson
   end
 
-  # POST /lessons
+  # POST /user/user_id/lessons
   def create
+    @user = User.find(params[:user_id])
     @lesson = Lesson.new(lesson_params)
 
     if @lesson.save
@@ -24,17 +26,26 @@ class LessonsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /lessons/1
+  # PATCH/PUT /user/user_id/lessons/lesson_id
+  def edit
+    @user = User.find(params[:user_id])
+    @lesson = Lesson.find(params[:lesson_id])
+  end
+  
   def update
-    if @lesson.update(lesson_params)
+    @user = User.find(params[:user_id])
+    @lesson = Lesson.find(lesson_params)
+    if @lesson.update_attributes(lesson_params)
       render json: @lesson
     else
       render json: @lesson.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /lessons/1
+  # DELETE /user/user_id/lessons/lesson_id
   def destroy
+    @user = User.find(params[:user_id])
+    @lesson = Lesson.find(params[:lesson_id])
     @lesson.destroy
   end
 
