2cb9300e2668727dc57a774c842b69250622e4aa
MikeyF53 <mikefang530@gmail.com>
2019-04-08 11:00:16 -0400

    moved folder with wireframes erd comp h

--- code_tutor/client/src/services/services.js
+++ code_tutor/client/src/services/services.js
@@ -2,22 +2,28 @@ const axios = require('axios');
 const BASE_URL = 'http://localhost:3000';
 
 
-//create user
-const createUser = async(data) => {
-  const resp = await axios.post(`${BASE_URL}/users`, data)
-  return resp.data
-}
+// //create user
+// const createUser = async(data) => {
+//   const resp = await axios.post(`${BASE_URL}/users`, data)
+//   return resp.data
+// }
+
+// //login user
+// const loginUser = async (data) => {
+//   const resp = await axios.post(`${BASE_URL}/users/login`, data);
+//   return resp.data;
+// }
+
+//show lessons
 
 
+//post lesson
 
 //show exercises
 
 //post exercise
 
-//show lessons
-
-//post lesson
 
 
 
-export { createUser }
+export { createUser, loginUser }
